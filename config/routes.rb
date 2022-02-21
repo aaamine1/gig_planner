bfbeae1b23322265bfdf5eda7d154c6af18dc9b1
amine achouri <ahmedamine.achouri@gmail.com>
2022-02-21 16:04:47 +0100

    Generated Venue Model, Venues controller and added Venues routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :venues
   devise_for :users
-  root to: 'venues#home'
+  root to: 'pages#home'
   # For details on the DSL  available within this file, see https://guides.rubyonrails.org/routing.html
 end
