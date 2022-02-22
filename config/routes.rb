80bd8add0513f6980daac4ffccc19698ea2bac99
GuyMattheuwsen <guy.mattheuwsen@gmail.com>
2022-02-22 17:44:59 +0100

    navbar pointer, correct booking save with userid linked

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :venues do
     resources :bookings, only: [:new, :create]
   end
+  # get 'bookings(/:booking_id)', to: 'pages#accept', as: :accept_booking
 end
