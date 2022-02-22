9403bcccaaf0fc732c7ed8f70495b551a079b24c
GuyMattheuwsen <guy.mattheuwsen@gmail.com>
2022-02-22 18:13:23 +0100

    prepare routes for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,16 @@ Rails.application.routes.draw do
   resources :venues do
     resources :bookings, only: [:new, :create]
   end
+  # index for all bookings for current_user
+  get '/dashboard', to: 'pages#dashboard', as: :dashboard
+  patch 'bookings/:id/accept', to: 'bookings#accept', as: :accept_booking
+  patch 'bookings/:id/reject', to: 'bookings#reject', as: :reject_booking
 end
+
+# current_user.bookings # (my bookings of other venues)
+# current_user.venues # (my venues)
+# current_user.venues.each do |venue|
+#   venue.bookings # bookings others made for a venue of mine
+#   # link_to 'accept this booking', accept_booking_path, method: :patch, status: "accepted"
+#   # link_to 'reject this booking', reject_booking_path, method: :patch, status: "rejected"
+# end
