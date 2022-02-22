5323ccc27c2ad32585540805808983a923b27ea8
amine achouri <ahmedamine.achouri@gmail.com>
2022-02-22 20:32:44 +0100

    App down from route conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   resources :venues do
     resources :bookings, only: [:new, :create]
   end
-  
+end
   # index for all bookings for current_user
-  get '/dashboard', to: 'pages#dashboard', as: :dashboard
-  patch 'bookings/:id/accept', to: 'bookings#accept', as: :accept_booking
-  patch 'bookings/:id/reject', to: 'bookings#reject', as: :reject_booking
-  # get 'bookings(/:booking_id)', to: 'pages#accept', as: :accept_booking
+  # get '/dashboard', to: 'pages#dashboard', as: :dashboard
+  # patch 'bookings/:id/accept', to: 'bookings#accept', as: :accept_booking
+  # patch 'bookings/:id/reject', to: 'bookings#reject', as: :reject_booking
+  # # get 'bookings(/:booking_id)', to: 'pages#accept', as: :accept_booking
 
 
 # current_user.bookings # (my bookings of other venues)
@@ -19,4 +19,3 @@ Rails.application.routes.draw do
 #   venue.bookings # bookings others made for a venue of mine
 #   # link_to 'accept this booking', accept_booking_path, method: :patch, status: "accepted"
 #   # link_to 'reject this booking', reject_booking_path, method: :patch, status: "rejected"
-# end
