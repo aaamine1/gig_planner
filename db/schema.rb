1d26c4542b34d570a3f7914667fd6ba861b149bb
Maximilien Lob <maximilien.lob@gmail.com>
2022-02-22 14:46:40 +0100

    add url to venues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_115136) do
+ActiveRecord::Schema.define(version: 2022_02_22_134342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_02_22_115136) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "name"
+    t.string "image_url"
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
