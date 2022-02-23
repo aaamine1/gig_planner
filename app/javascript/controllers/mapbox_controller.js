8e716037de4b6f5806521b199dadc3bfce7f6fc8
amine achouri <ahmedamine.achouri@gmail.com>
2022-02-23 14:14:18 +0100

    Finished map and index page layout

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -12,7 +12,8 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v11"
+
     })
   }
 }
