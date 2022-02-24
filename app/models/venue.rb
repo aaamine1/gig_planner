dd6c1d297cd510c06439bfd698a4063c96d82982
Maximilien Lob <maximilien.lob@gmail.com>
2022-02-24 17:07:47 +0100

    master switch

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,7 @@
 class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
