e543dd018192b1f57a63cdd3c1cd845492649e8d
GuyMattheuwsen <guy.mattheuwsen@gmail.com>
2022-02-24 22:36:10 +0100

    further map integration with markers on index page

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,7 @@ Rails.application.routes.draw do
   patch 'bookings/:id/reject', to: 'bookings#reject', as: :reject_booking
 end
 
-
-  # get 'bookings(/:booking_id)', to: 'pages#accept', as: :accept_booking
-
 # current_user.bookings # (my bookings of other venues)
 # current_user.venues # (my venues)
 # current_user.venues.each do |venue|
 #   venue.bookings # bookings others made for a venue of mine
-#   # link_to 'accept this booking', accept_booking_path, method: :patch, status: "accepted"
-#   # link_to 'reject this booking', reject_booking_path, method: :patch, status: "rejected"
-
-
-# <%= link_to 'Accept this booking', accept_booking_path, method: :patch, status: "accepted" %>
-# <%= link_to 'Reject this booking', reject_booking_path, method: :patch, status: "rejected" %>
